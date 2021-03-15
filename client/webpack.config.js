1600f5e775f73bf6c1d602b98b463ffddfcb8a95
mcverter <mitchell.verter@gmail.com>
2021-03-15 10:01:14 -0400

    fixing sass

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,30 +1,40 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
   entry: {
-    app: ["@babel/polyfill", "./src/app.js"],
+    app: ['@babel/polyfill', './src/app.js'],
   },
   output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "app.bundle.js",
+    path: path.resolve(__dirname, 'build'),
+    filename: 'app.bundle.js',
   },
   module: {
     rules: [
       {
         test: /\.js?$/,
         exclude: /node_modules/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
         query: {
-          presets: ["@babel/preset-env"],
+          presets: ['@babel/preset-env'],
         },
       },
       {
         test: /\.css$/,
-        use: ["raw-loader"],
+        use: ['raw-loader'],
       },
       {
         test: /\.scss$/,
-        use: ["sass-to-string"],
+        use: [
+          'sass-to-string',
+          {
+            loader: 'sass-loader',
+            options: {
+              sassOptions: {
+                outputStyle: 'compressed',
+              },
+            },
+          },
+        ],
       },
     ],
   },
